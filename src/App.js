1673deef31f9dc96297b860c32fc74e8901bcab9
yeakode <hiwrld1010@gmail.com>
2023-05-01 22:10:13 +0900

    app.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Counsel from "./Counsel";
+
+function App() {
+  return (
+    <div className="App">
+      <Counsel />
+    </div>
+  );
+}
+
+export default App;
