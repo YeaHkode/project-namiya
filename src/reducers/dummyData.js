eb65df982f2e0742ed46851d257378cadda636a7
yeakode <hiwrld1010@gmail.com>
2023-05-02 23:58:16 +0900

    dummyData.js

---
+++ src/reducers/dummyData.js
@@ -0,0 +1,20 @@
+export const dummyData = {
+  counselorList: [
+    {
+      id: 1,
+      title: "코를 돌려주세요ㅜㅜ",
+      content: "비염 치료법",
+    },
+    {
+      id: 5,
+      title: "you is much easyer",
+      content: "so easyer",
+    },
+    {
+      id: 2,
+      title: "weathers are many goods",
+      content: "real goods",
+    },
+  ],
+};
+export default dummyData;
